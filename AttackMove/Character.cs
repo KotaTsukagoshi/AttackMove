3a744a4fc1343b58744732bd37db574cd7e8a4be
KotaTsukagoshi <uni202406_12@telework.to>
2024-08-20 10:30:15 +0900

    CharacterClass

---
+++ AttackMove/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttackMove
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public IAttack AttackType { get; set; }
+
+        public Character(string name, IAttack attackType)
+        {
+            Name = name;
+            AttackType = attackType;
+        }
+
+        public string PerformAttack()
+        {
+            return $"{Name}は{AttackType.Attack()}";
+        }
+    }
+}
