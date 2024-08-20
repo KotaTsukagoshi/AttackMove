f4b6aa914e8be85870f483751929641e3af4dbc6
MitsuruKanazawa <uni202406_07@telework.to>
2024-08-20 10:14:32 +0900

    インターフェース追加

--- AttackMove/Form1.cs
+++ AttackMove/Form1.cs
@@ -12,9 +12,35 @@ namespace AttackMove
 {
     public partial class Form1 : Form
     {
+        private string selectedAttack;
+
         public Form1()
         {
             InitializeComponent();
+
+            comboBox1.Items.Add("剣");
+            comboBox1.Items.Add("魔法");
+            comboBox1.Items.Add("弓");
+
+            selectedAttack = "剣";  // 初期選択
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "剣":
+                    selectedAttack = "剣で攻撃した！";
+                    break;
+                case "魔法":
+                    selectedAttack = "魔法を使った！";
+                    break;
+                case "弓":
+                    selectedAttack = "弓で攻撃した！";
+                    break;
+            }
+
+            MessageBox.Show(selectedAttack);
         }
     }
 }
